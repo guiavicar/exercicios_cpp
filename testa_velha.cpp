fa5b57720f6889f493171bf0850c23de572a3b1c
Guilherme <guiavicar777@gmail.com>
2023-10-03 03:52:40 -0300

    teste de verificacao vertical e correcao de bug (combinacoes de casas vazias era confundida com casas preenchidas)

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -11,12 +11,22 @@
 
 
 TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste1[3][3]= {   { 2, 0, 1 }, 
-	                      { 2, 0, 1 },
-						  { 0, 2, 1 }
+	SECTION("testa horizontal"){
+		int teste_combinacao_horizontal[3][3]= { 
+		 				  { 0, 0, 0 }, 
+	                      { 2, 2, 2 },
+						  { 0, 0, 0 }
 					  };
-    REQUIRE( VerificaVelha(teste1) == 1 );
+    	REQUIRE( VerificaVelha(teste_combinacao_horizontal) == 2 );
+	};
 
-
+	SECTION("Testa vertical", "[single-file]"){
+		int teste_combinacao_vertical[3][3]={
+						  {1, 0, 0},
+						  {1, 0, 0},
+						  {1, 0, 0}
+					  };
+		REQUIRE( VerificaVelha(teste_combinacao_vertical) == 1 );
+	}
 }
  
--- velha.cpp
+++ velha.cpp
@@ -2,7 +2,7 @@
  * \file  velha.cpp
  */
 
- 
+#include <iostream>
 #include "velha.hpp"
 
 /** 
@@ -11,11 +11,27 @@
  * @param  velha descreve o parametro
  * 
  *  Descrever o que a funcao faz
- */ 
+ */
 
 int VerificaVelha( int velha[3][3] )
 {
-	return 1; /*!< retorna zero para teste */
-}
+	for (int i=0 ; i<3 ; i++){
 
+		if (velha[i][0] == velha[i][1] && velha[i][0] == velha[i][2] && velha[i][0]!=0)
+		{  /* verificando se existe uma combinaçao na horizontal*/
+			
+			return (velha[i][1]); 	/*retorna o tipo da combinacao vencedora*/
+		}
+	} 
+	
+	for (int j=0 ; j<3 ; j++){
+
+		if (velha[0][j] == velha[1][j] && velha[0][j] == velha[2][j] && velha[0][j]!=0)
+		{ /* verificando se existe uma combinaçao na vertical*/
+
+			return(velha[1][j]); /*retorna o tipo da combinacao vencedora */
+		}
+
+	}
+}
 
